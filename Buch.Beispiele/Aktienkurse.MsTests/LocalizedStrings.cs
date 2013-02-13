3ce3da377af93d6f7bf5173f0401fedb892d16ef
Gregor Woiwode <gregor.woiwode@gmail.com>
2013-02-13 19:54:10 +0100

    Projekt für MS-Test Unit-Testing angelegt.

---
+++ Buch.Beispiele/Aktienkurse.MsTests/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Aktienkurse.MsTests.Resources;
+
+namespace Aktienkurse.MsTests
+{
+  /// <summary>
+  /// Provides access to string resources.
+  /// </summary>
+  public class LocalizedStrings
+  {
+    private static AppResources _localizedResources = new AppResources();
+
+    public AppResources LocalizedResources { get { return _localizedResources; } }
+  }
+}
---
+++ Buch.Beispiele/Aktienkurse.MsTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Aktienkurse.MsTests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
