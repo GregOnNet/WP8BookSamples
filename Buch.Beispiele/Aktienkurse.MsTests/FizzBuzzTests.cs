ac4b0eee656b4a9e73ac14008e43047e6cb988b5
Gregor Woiwode <gregor.woiwode@gmail.com>
2013-02-17 09:57:58 +0100

    Testklasse FizzBuzzTests angelegt.

---
+++ Buch.Beispiele/Aktienkurse.MsTests/FizzBuzzTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Aktienkurse.MsTests
+{
+  [TestClass]
+  public class FizzBuzzTests
+  {
+    [TestMethod]
+    public void Processing_a_number_divisible_by_3_it_should_return_Fizz()
+    {
+      int input = 6;
+      var fizzbuzz = new FizzBuzz();
+
+      var result = FizzBuzz.Process(input);
+
+      Assert.AreEqual("Fizz", result);
+    }
+  }
+
+  public class FizzBuzz
+  {
+    public static string Process(int input)
+    {
+      return "Fizz";
+    }
+  }
+}
--- Buch.Beispiele/Aktienkurse.MsTests/MainPage.xaml.cs
+++ Buch.Beispiele/Aktienkurse.MsTests/MainPage.xaml.cs
@@ -8,14 +8,12 @@ namespace Aktienkurse.MsTests
 {
   public partial class MainPage : PhoneApplicationPage
   {
-    // Constructor
     public MainPage()
     {
       InitializeComponent();
 
       var wrapper = new TestExecutorServiceWrapper();
       new Thread(new ServiceMain((param0, param1) => wrapper.SendMessage((ContractName)param0, param1)).Run).Start();
-
     }
   }
 }
